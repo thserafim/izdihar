f2658ea35fd357e81f0a9abfa9ff2b9dffcb01ef
thiagoseraffim@hotmail.com <thiagoseraffim@hotmail.com>
2026-03-19 22:15:04 -0300

    alteração vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-    allowedHosts: ['all'],
+    allowedHosts: 'all',
   },
   server: {
     host: '0.0.0.0',
